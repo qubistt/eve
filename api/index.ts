fda8503cca635d54690e71c7b905c3f56a371708
qubist <78350166+qubistt@users.noreply.github.com>
2024-11-27 09:30:36 +0000

    added new commands

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,19 @@ const {
 	verifyKey,
 } = require("discord-interactions");
 const getRawBody = require("raw-body");
+
+const SLAP_COMMAND = {
+	name: 'Slap',
+	description: 'Sometimes you gotta slap a person with a large trout',
+	options: [
+	  {
+		name: 'user',
+		description: 'The user to slap',
+		type: 6,
+		required: true,
+	  },
+	],
+  };
  
 const INVITE_COMMAND = {
 	name: "Invite",
