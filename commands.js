e1fe64281524aa7a3d909a9c7c0bc825b015cb41
qubist <78350166+qubistt@users.noreply.github.com>
2024-11-27 10:07:50 +0000

    implemented fastify for serverless function backend

---
+++ commands.js
@@ -0,0 +1,17 @@
+export const SLAP_COMMAND = {
+	name: "Slap",
+	description: "Sometimes you gotta slap a person with a large trout",
+	options: [
+		{
+			name: "user",
+			description: "The user to slap",
+			type: 6,
+			required: true,
+		},
+	],
+};
+ 
+export const INVITE_COMMAND = {
+	name: "Invite",
+	description: "Get an invite link to add the bot to your server",
+};
---
+++ register.js
@@ -0,0 +1,22 @@
+import fetch from "node-fetch";
+import { SLAP_COMMAND, INVITE_COMMAND } from "./server";
+ 
+const response = await fetch(
+	`https://discord.com/api/v8/applications/${process.env.APPLICATION_ID}/commands`,
+	{
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bot ${process.env.TOKEN}`,
+		},
+		method: "PUT",
+		body: JSON.stringify([SLAP_COMMAND, INVITE_COMMAND]),
+	},
+);
+ 
+if (response.ok) {
+	console.log("Registered all commands");
+} else {
+	console.error("Error registering commands");
+	const text = await response.text();
+	console.error(text);
+}
