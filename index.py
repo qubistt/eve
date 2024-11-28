ff3a11eeea030554bf9c4158b1c8dff77a2e5116
qubist <78350166+qubistt@users.noreply.github.com>
2024-11-28 06:08:49 +0000

    huge changes: introducing AI chat

--- index.py
+++ index.py
@@ -1,20 +1,43 @@
 import discord
+from discord import app_commands
+import google.generativeai as genai
+import os
+from google.generativeai import upload_file, GenerativeModel
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-@client.event
+
+gemini_api = os.getenv('GEMINI_API')
+token = os.getenv('TOKEN')
+
+genai.configure(api_key="")
+model = genai.GenerativeModel("models/gemini-1.5-pro")
+
+@client.event  
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    try:
+        await tree.sync()
+        print("Commands synced successfully.")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content:
+        rules = 'you are a 19 year old. talk in slightly gen-z terms, little to none punctuation in your sentences and no first letter capitalisation. sometimes you wanna show surprise or extreme emotion so you make any suitable word ALL CAPS! if you see two or more people having a normal conversation, interject humorously every once in a while, communicate using brainrot whenever possible, reply and keep it relevant dont add extra stuff.'
+        response = model.generate_content(f'{rules}, {message.content}')
+        print(response)
+        print('==>', response.text)
+        if len(response.text)>1999:
+            response = model.generate_content(f'shorten this: {message.content}')
+        await message.channel.send(response.text)
 
-client.run('your token here')
+client.run("")
