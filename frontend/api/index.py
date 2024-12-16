85f33fd829d079aecfd360b644a065c814371278
qubist <78350166+qubistt@users.noreply.github.com>
2024-12-16 22:39:07 +0530

    split frontend and backend channels

---
+++ frontend/api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'About'
+
+if __name__ == '__main__':
+    app.run(debug=True)
